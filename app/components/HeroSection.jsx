43fa4df778e77ee884554aa18527d0787babfd6b
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-02-06 14:32:14 -0500

    Add resume

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ export const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-black mb-4 text-4xl sm;text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-              Hi, I'm {" "}
+              Hi, I'm{" "}
             </span>
             <br></br>
             <TypeAnimation
@@ -35,19 +35,26 @@ export const HeroSection = () => {
             optimal performance and user satisfaction.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-50 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+            <Link
+              href="/contactMe"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
               Hire Me
-            </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-              <span className="block hover:bg-slate-800 rounded full px-5 py2">
+            </Link>
+            <Link
+              href="/Giselle'sResume.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#CDFADB] w-[250px] h-[250px] lg:w-[500px] lg:h-[500px] relative">
-            <Image src="/minime.png"
+            <Image
+              src="/minime.png"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={250}
               height={250}
