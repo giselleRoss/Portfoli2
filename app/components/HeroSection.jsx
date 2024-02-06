9e89464131fae4c57b2f11f2c78d909822935b4b
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-02-06 14:48:03 -0500

    Add new color

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -38,13 +38,13 @@ export const HeroSection = () => {
           <div>
             <Link
               href="/contactMe"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-[#CDFADB] to-[#CDFADB] text-white"
             >
               Hire Me
             </Link>
             <Link
               href="/Giselle'sResume.pdf"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-[#CDFADB] to-[#CDFADB] text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
